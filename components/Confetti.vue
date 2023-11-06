88eaae4bdcde15e597215634e221d8b5ae564fa7
JoseMa Santos <josantos@domingoalonsogroup.com>
2023-11-06 21:22:04 +0100

    Add confetti animation and vue-rewards package

---
+++ components/Confetti.vue
@@ -0,0 +1,25 @@
+<script setup>
+  import { useReward } from 'vue-rewards'
+  import { onMounted } from 'vue'
+
+  const config = {
+    startVelocity:10,
+    spread:180,
+    elementCount:100
+    // etc...
+    // you can make this reactive instead etc.
+  }
+
+  // has to be in onMounted, otherwise the element won't be found.
+  // This will trigger the animation immediately.
+  
+  // or you can trigger the animation onclick
+  // I have renamed reward to balloonsReward etc.
+
+  const { reward: balloonsReward, isAnimating: isBalloonsAnimating } = useReward('some-id', 'confetti');
+</script>
+
+<template>
+  <span id="some-id" @click="balloonsReward"> Let's celebrate! </span>
+  
+</template>
